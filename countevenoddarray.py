f6199ad623e06ca8c872124a94f7b43311db5865
Ramaneesh88 <nagaramaneeshmaari@gmail.com>
2026-07-23 16:34:47 +0530

    11 problem

---
+++ countevenoddarray.py
@@ -0,0 +1,13 @@
+def even_odd_count(arr):
+    count1=0
+    count2=0
+    if len(arr)>0:
+        for num in arr:
+            if num%2==0:
+                count1+=1
+            else:
+                count2+=1
+        return f"even:{count1}, odd:{count2}"
+    return "Array is empty"
+a=list(map(int,input("Enter the numbers:").split()))
+print(even_odd_count(a))
